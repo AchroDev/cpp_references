32031a1322ad8e2f81c69ab8dd62df55e93ede30
AchroDev <achrodev@proton.me>
2024-09-14 18:55:14 -0500

    Created a variable, and reference examples

--- main.cpp
+++ main.cpp
@@ -7,6 +7,13 @@
 
 int main() {
     
+    int a  = 5; // just a variable
+    // The '&' operator is used to obtain the memory address of a variable below.
+    int* b = &a; // b is being declared as a pointer variable that points to the direct memory location of 'a'.
+    // The '&' operator is used to create a reference to a variable in the second case.
+    int& ref = a; // ref is being declared as a reference to the same memory location as the integer 'a'. Essentially an alias.
+    
+    
     LOG("AchroDev");
     
     std::cin.get();
