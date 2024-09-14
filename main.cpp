1d53b77ab3b7f34b2f2008c0eb7ae71a918b021a
AchroDev <achrodev@proton.me>
2024-09-14 18:41:49 -0500

    Added comment

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 
 #define LOG(x) std::cout << x << std::endl;
 
+// References are usually just pointers in disquise
+// Some delicious syntactical sugar.
+
 int main() {
     
     LOG("AchroDev");
