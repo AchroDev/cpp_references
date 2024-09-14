da9db019c03ca18d7ad889fc850e81bfc1fe6b98
AchroDev <achrodev@proton.me>
2024-09-14 18:58:19 -0500

    Added comment about key difference between pointers and references

--- main.cpp
+++ main.cpp
@@ -5,6 +5,13 @@
 // References are usually just pointers in disquise
 // Some delicious syntactical sugar.
 
+/* 
+ * KEY DIFFERENCES BETWEEN A POINTER AND A REFERNCE
+ * Pointer: A pointer stores the memory address of a variable. You can use it to indirectly access and modify 
+   the value at that address.
+ * Reference: A reference is an alias for an existing variable. It doesn't store an address but directly refers to
+   the same memory location as the original variable. 
+*/
 int main() {
     
     int a  = 5; // just a variable
