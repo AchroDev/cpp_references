93d74afe0b870e8541759284aeb3b0767aae54e8
AchroDev <achrodev@proton.me>
2024-09-14 22:38:13 -0500

    Added reference to 'a' in call to Increment and added appropriate dereference to modify the intended value.

--- main.cpp
+++ main.cpp
@@ -15,15 +15,16 @@
 
 // Function for example of how this doesn't use a reference and direct copies the variable fed to it, i.e 'a' or 5.
 // This causes the increment to not happen as it doesn't have "ownership" or access to the data.
-void Increment(int value)
+void Increment(int* value)
 {
-    value++;
+    // encapsulating '*value' in parenthesis causes the dereference to happen first.
+    (*value)++; // dereferencing to increment the value stored in the variable and not the memory address
 }
 
 int main() {
     
     int a  = 5; // just a variable
-    Increment(a);
+    Increment(&a); // now using '&' to direct access the memory value
     LOG(a);
     
     std::cin.get();
